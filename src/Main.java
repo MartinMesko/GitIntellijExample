15bfb2a27d55ba7d2d8949d97672ac9520488b7a
ezekiel55 <alegremato@gmail.com>
2024-02-26 11:25:25 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,5 @@ public class Main {
     public static void main(String[] args) {
         //This is comment
         System.out.println("Hello world");
-
-        System.out.println("H");
-
-        System.out.println("Hell");
     }
 }
