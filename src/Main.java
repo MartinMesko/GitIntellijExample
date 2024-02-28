3d259aaee2ecaec8aa1e62ea714e8e886f3e4256
ezekiel55 <alegremato@gmail.com>
2024-02-28 13:32:48 +0100

    Added greeting in English

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         //This is comment
         System.out.println("Hello world");
+
+        System.out.println("Hi everybody");
     }
 
 }
